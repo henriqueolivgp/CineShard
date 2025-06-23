1263b32feb086a1558be82186bb747f25ad3905c
henriqueolivgp <henrique.o@ipvc.pt>
2025-06-23 16:32:14 +0100

    delete all old pieces of code and remake home page

--- src/web/src/Pages/home.js
+++ src/web/src/Pages/home.js
@@ -1,19 +1,13 @@
-import { Power, ShieldMinus } from "lucide-react";
-// import { Sidebar } from "../components/home/sidebar";
 import { Outlet } from "react-router-dom";
-import { Link } from 'react-router-dom'
-import { useAuth } from "../hooks/AuthHook";
 import { Sidebar } from "../components/home/sidebar";
-import { AsideBar } from "../components/home/aside-bar";
 
 export default function Home() {
     return (
         <>
-            <div className="content">
-                <AsideBar />
-                <Sidebar />
+            <Sidebar />
+            <div className="flex flex-col justify-center p-4 sm:ml-64 ">
+                <Outlet />
             </div>
-
         </>
     );
 }
